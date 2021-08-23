2ac913c66a2dd5b64dd61c56d793618844dbb6f5
Frederico Araujo <fredericoaraujo@gmail.com>
2021-08-23 12:48:03 -0300

    Ponto flutuante e formatação de casas decimais

---
+++ PontoFlutuante/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Globalization;
+
+namespace PontoFlutuante
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x = 10.35784;
+            Console.WriteLine(x);
+            Console.WriteLine(x.ToString("F2"));
+            Console.WriteLine(x.ToString("F4"));
+            Console.WriteLine(x.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
